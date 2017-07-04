22dde8fa2a3ebe669f05141ad91a684f92299b7c
sangkwon <blue03183@playauto.co.kr>
2017-07-05 00:16:57 +0900

    커밋 테스트

---
+++ storage/framework/views/96feb31f735d1f69a915bb78353848a22ad894ad.php
@@ -0,0 +1,7 @@
+<?php $__env->startSection('script'); ?>
+    ##parent-placeholder-cb5346a081dcf654061b7f897ea14d9b43140712##
+    <script>
+        alert("저는 조각뷰의 script 섹션입니다.");
+    </script>
+<?php $__env->stopSection(); ?>
+
